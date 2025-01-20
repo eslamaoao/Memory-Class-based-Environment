4667d69b16056f5db5c42ec0d5c1fdf846870541
Eslam Ashraf <92990276+eslamaoao@users.noreply.github.com>
2025-01-20 15:01:11 +0200

    Update Mem_tb.sv

--- Testbench/Mem_tb.sv
+++ Testbench/Mem_tb.sv
@@ -44,36 +44,3 @@ always #(clock_period / 2) intf1.CLK = ~intf1.CLK;
 endmodule
 
 
-/*driver1 = new ();
-monitor1 = new ();
-
-driver1.clk = 1;
-driver1.rst = 0;
-driver1.en = 1;
-driver1.w_r = 1; // Write
-driver1.address = 4'h3;
-driver1.data_in = 32'd120;
-
-driver1.driv(intf1);
-#10;
-driver1.rst = 1;
-driver1.driv(intf1);
-#30
-driver1.w_r = 0; // Write
-#30
-$display($time ,"ns data_out = %0d",monitor1.data_out);
-	
-$display($time ,"ns valid_out = %0d",monitor1.valid_out);
-$display($time ,"ns valid_out = %0d",DUT.Valid_Out);
-
- driver1.driv(intf1);
- 
- #30
- $display($time ,"ns data_out = %0d",monitor1.data_out);
-	
-$display($time ,"ns valid_out = %0d",monitor1.valid_out);
- monitor1.mon(intf1);
- $display(  $time ,"ns data_out = %0d",monitor1.data_out);
-	
-$display( $time ,"ns valid_out = %0d",monitor1.valid_out);
- #200;*/
